262aa58c23e2d10ac203c40a1ad703c39db09d76
samtalebi <samtalebi84@gmail.com>
2020-05-27 22:20:15 +0200

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+//import Authors from './components/Authors';
+//import Books from './components/Books';
+import NewBook from './components/NewBook';
+import { CREATE_BOOK, ALL_AUTHORS, ALL_BOOKS } from './services/query';
+
+const App = () => {
+  const [page, setPage] = useState('authors');
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <NewBook show={page === 'add'} />
+    </div>
+  );
+};
+
+export default App;
+
+/*
+<Authors show={page === 'authors'} />
+
+<Books show={page === 'books'} />
+*/
